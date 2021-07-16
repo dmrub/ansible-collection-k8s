9fa1630711d1a538a8ee3b255afccdd1a6ea1a19
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2021-07-16 16:47:24 +0200

    Improvements

--- roles/k8s_multi_cluster_dir/files/deploy-cluster.sh
+++ roles/k8s_multi_cluster_dir/files/deploy-cluster.sh
@@ -7,13 +7,17 @@ THIS_DIR=$( cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-if [[ -e "$KUBESPRAY_DIR/disable-firewall.yml" ]]; then
-    run-ansible-playbook "$KUBESPRAY_DIR/disable-firewall.yml" "$@"
-fi
+#if [[ -e "$KUBESPRAY_DIR/disable-firewall.yml" ]]; then
+#    run-ansible-playbook "$KUBESPRAY_DIR/disable-firewall.yml" "$@"
+#fi
+#
+#if [[ -e "$KUBESPRAY_DIR/disable-swap.yml" ]]; then
+#    run-ansible-playbook "$KUBESPRAY_DIR/disable-swap.yml" "$@"
+#fi
 
-if [[ -e "$KUBESPRAY_DIR/disable-swap.yml" ]]; then
-    run-ansible-playbook "$KUBESPRAY_DIR/disable-swap.yml" "$@"
-fi
+run-ansible-playbook "$THIS_DIR/../playbooks/disable-firewall.yml" "$@"
+
+run-ansible-playbook "$THIS_DIR/../playbooks/disable-swap.yml" "$@"
 
 run-ansible all -m setup
 
--- roles/k8s_multi_cluster_dir/files/reset-cluster.sh
+++ roles/k8s_multi_cluster_dir/files/reset-cluster.sh
@@ -7,6 +7,6 @@ THIS_DIR=$( cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-run-ansible-playbook \
-    "$THIS_DIR/../playbooks/check-connectivity.yml" \
-    "$@"
+run-ansible all -m setup
+
+run-ansible-playbook "$KUBESPRAY_DIR/reset.yml" --become --become-user=root "$@"
